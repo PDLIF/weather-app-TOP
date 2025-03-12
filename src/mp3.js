dc71bd62674dca0c78ae38a75c02b631b868a33c
astuarykig <akvsevolod@icloud.com>
2025-03-12 11:10:31 +0500

    make player volume tab draggable

--- src/mp3.js
+++ src/mp3.js
@@ -13,6 +13,8 @@ const songs = ['Aphex Twin - 3'];
 
 let songIndex = 0;
 
+let isDragging = false;
+
 loadSong(songs[songIndex]);
 
 function loadSong(song) {
@@ -42,25 +44,56 @@ function updateVolume(e) {
 }
 
 function setVolume(e) {
-    const width = this.clientWidth;
-    const clickX = e.offsetX;
-    const newVolume = clickX / width;
+    const width = volumeContainer.clientWidth;
+    let clickX = e.offsetX;
+
+    if (e.type === "mousemove") {
+        clickX = e.clientX - volumeContainer.getBoundingClientRect().left;
+    }
+    
+    let newVolume = clickX / width;
+    newVolume = Math.max(0, Math.min(1, newVolume));
 
     audio.volume = newVolume;
 }
+
+function startDragging(e) {
+    isDragging = true;
+    setVolume(e);
+}
+
+function stopDragging() {
+    isDragging = false;
+}
+
+function dragging(e) {
+    if (!isDragging) return;
+    setVolume(e);
+}
+
+// Event Listeners
+
+volumeContainer.addEventListener('mousedown', startDragging);
+document.addEventListener('mouseup', stopDragging);
+document.addEventListener('mousemove', dragging);
+
+audio.addEventListener('volumechange', () => {
+    volumeBar.style.width = `${audio.volume * 100}%`;
+});
+
+
+
+
+
+
 
 // Event listeners
 
 playBtn.addEventListener('click', () => {
     const isPlaying = playerContainer.classList.contains('play');
-
-    if (isPlaying) {
-        pauseSong();
-    } else {
-        playSong();
-    }
+    isPlaying ? pauseSong() : playSong();
 });
 
-volumeContainer.addEventListener('click', setVolume);
+// volumeContainer.addEventListener('click', setVolume);
 audio.addEventListener('volumechange', updateVolume)
 
